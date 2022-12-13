99dd6575327cd9e5f3d5744c1afb240f021afb35
javaprovip <sergiokravchenko@gmail.com>
2022-12-13 20:51:24 +0200

    controlled search component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Navbar, Container} from 'react-bootstrap';
+
+const navbarStyle = {
+    backgroundColor: 'lightblue'
+}
+
+const Header = () => {
+    return (
+        <Navbar style={navbarStyle} variant="light">
+            <Container>
+              <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
+            </Container>
+        </Navbar>
+
+    )
+};
+
+export default Header;
+
