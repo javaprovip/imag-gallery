eb184ba2c462759ea40a6fac5f471685b6d7b8f0
javaprovip <sergiokravchenko@gmail.com>
2022-12-14 18:40:59 +0200

    fixed code with linter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,30 +7,28 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState('');
-  
+
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-       .then((res) => res.json())
-       .then((data) => {
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data);
-       })
+      })
 
-       .catch((err) => {
+      .catch((err) => {
         console.log(err);
-       })
-       setWord('');
-  }
-
-  
-
-  
+      });
+    setWord('');
+  };
 
   return (
     <div className="App">
-      <Header/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Header />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import {Navbar, Container} from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 
 const navbarStyle = {
-    backgroundColor: 'lightblue'
-}
+  backgroundColor: 'lightblue',
+};
 
 const Header = () => {
-    return (
-        <Navbar style={navbarStyle} variant="light">
-            <Container>
-              <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
-            </Container>
-        </Navbar>
-
-    )
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
-
